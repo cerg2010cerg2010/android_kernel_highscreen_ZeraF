f5f11da8c16b03b734a3a0fd92c1651dcf5e9987
cerg2010cerg2010 <cerg2010cerg2010@users.noreply.github.com>
2017-04-11 19:12:48 +0000

    Backlight fix

--- mediatek/custom/ztenj72_we_72_kk/kernel/leds/mt65xx/cust_leds.c
+++ mediatek/custom/ztenj72_we_72_kk/kernel/leds/mt65xx/cust_leds.c
@@ -3,8 +3,8 @@
 #include <mach/mt_pwm.h>
 
 #include <linux/kernel.h>
-#include <mach/pmic_mt6329_hw_bank1.h> 
-#include <mach/pmic_mt6329_sw_bank1.h> 
+#include <mach/pmic_mt6329_hw_bank1.h>
+#include <mach/pmic_mt6329_sw_bank1.h>
 #include <mach/pmic_mt6329_hw.h>
 #include <mach/pmic_mt6329_sw.h>
 #include <mach/upmu_common_sw.h>
@@ -13,142 +13,81 @@
 #include <linux/delay.h>
 #include <mach/irqs.h>
 
-#define LCD_PWM_MOD_VALUE 1024
+#define LCD_PWM_MOD_VALUE 255
 
 #ifndef GPIO_LCD_PULSE_CONTRL
 #define GPIO_LCD_PULSE_CONTRL GPIO134
 #endif
 
-static unsigned isFirstOn=1;
-//extern int mtkfb_set_backlight_level(unsigned int level);
-//extern int mtkfb_set_backlight_pwm(int div);
-extern int disp_bls_set_backlight(unsigned int level);
-
-#define ERROR_BL_LEVEL 0xFFFFFFFF
-
-unsigned int brightness_mapping(unsigned int level)
-{
-	return ERROR_BL_LEVEL;
-}
-
-/*
-unsigned int brightness_mapping(unsigned int level)
-{
-	unsigned int mapped_level;
-
-	mapped_level = level;
-
-	return mapped_level;
-}
-*/
-unsigned int Cust_SetBacklight(int level, int div)
-{
-	//mtkfb_set_backlight_pwm(div);
-	//mtkfb_set_backlight_level(brightness_mapping(level));
-	disp_bls_set_backlight(brightness_mapping(level));
-	return 0;
-}
-
 static int custom_disp_bls_set_backlight(unsigned int  value)
 {
-	int err = 0xff;
-	unsigned long flags;
-	unsigned long duty_mod= 0;
-	unsigned long brightness= value;
-	unsigned int i= 0;
-	unsigned int  brightness_count = 0;
-	static unsigned int s_bBacklightOn = 1;
-	static unsigned int	g_ledCurrPulseCount = 0;
-	printk("custom_disp_bls_set_backlight value=%d \n",value);
-	//gpio_direction_output(143,1);
-	mt_set_gpio_mode(GPIO_LCD_PULSE_CONTRL, GPIO_MODE_00);
-	mt_set_gpio_dir(GPIO_LCD_PULSE_CONTRL, GPIO_DIR_OUT);
+        unsigned int brightness;
+        unsigned int brightness_count;
+        unsigned int i;
+        unsigned long flags;
+        static unsigned int s_bBacklightOn;
+        static unsigned int g_ledCurrPulseCount;
 
-	if(value > LCD_PWM_MOD_VALUE)
-		value = LCD_PWM_MOD_VALUE;
+        value = LCD_PWM_MOD_VALUE * value >> 10;
 
-	if(value < 0)
-		value = 0;
-	if(isFirstOn) {
-		mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
-		s_bBacklightOn = 0;
-		g_ledCurrPulseCount = 0;
-		mdelay(2);
-		isFirstOn=0;
-	}
+        if (value > LCD_PWM_MOD_VALUE)
+                brightness = 0;
 
-	brightness= value;
-	duty_mod = (value << 8) | LCD_PWM_MOD_VALUE;
-	if (value == 0)
-	{
-		mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
+        if (value == 0) {
+                mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
+                s_bBacklightOn = 0;
+                g_ledCurrPulseCount = 0;
+                mdelay(2);
+        }
+        else
+        {
+                brightness = (LCD_PWM_MOD_VALUE - value) >> 3;
+		printk("brightness is %d\n", brightness);
 
-		s_bBacklightOn = 0;
-		g_ledCurrPulseCount = 0;
-		mdelay(2);
-	}
-	else
-	{
-		brightness =(1024 -brightness) /33;
+                if (s_bBacklightOn) {
+                        if (g_ledCurrPulseCount > brightness)
+                                brightness_count = brightness + 32 - g_ledCurrPulseCount;
+                        else if (g_ledCurrPulseCount < brightness)
+                                brightness_count = brightness - g_ledCurrPulseCount;
+                        else
+                                return 0;
 
-	        printk("brightness is %d\n",brightness);
+                        local_irq_save(flags);
+                        for(i = 0; i < brightness_count ; i++)
+                        {
+                                mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
+                                udelay(1);
+                                mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
+                                udelay(1);
+                        }
+                        local_irq_restore(flags);
+                }
+                else
+                {
+                        local_irq_save(flags);
+                        mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
+                        udelay(40);
+                        for(i = 0; i < brightness; i++)
+                        {
+                                mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
+                                udelay(1);
+                                mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
+                                udelay(1);
+                        }
+                        local_irq_restore(flags);
+                }
 
-#if 1
-		if (s_bBacklightOn)
-		{
-			if (g_ledCurrPulseCount > brightness)
-			{
-				brightness_count = brightness+32-g_ledCurrPulseCount;
-			}
-			else if (g_ledCurrPulseCount < brightness)
-			{
-				brightness_count = brightness - g_ledCurrPulseCount;
-			}
-			else
-			{
-			        return 0;
-			}
+                g_ledCurrPulseCount = brightness;
+                s_bBacklightOn = 1;
+        }
+        return 0;
 
-
-			local_irq_save(flags);
-			for(i=0 ; i < brightness_count ; i++)
-    			{
-    				mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
-				udelay(1);
-				mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
-				udelay(1);
-    			}
-			local_irq_restore(flags);
-		}
-        	else
-        	{
-			brightness_count = brightness;
-			local_irq_save(flags);
-			mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
-			udelay(40);
-        		for(i=0 ; i < brightness_count; i++)
-        		{
-        			mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ZERO);
-        			udelay(1);
-        			mt_set_gpio_out(GPIO_LCD_PULSE_CONTRL, GPIO_OUT_ONE);
-        			udelay(1);
-        		}
-			local_irq_restore(flags);
-        	}
-
-        	g_ledCurrPulseCount = brightness;
-        	s_bBacklightOn = 1;
-
-#endif
-	}
-
-	return 0;
 }
 
 
 static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
-	{"red",               MT65XX_LED_MODE_PMIC,MT65XX_LED_PMIC_NLED_ISINK0, {0}},
-	{"green",             MT65XX_LED_MODE_PMIC,MT65XX_LED_PMIC_NLED_ISINK1, {0}},
+	{"red",               MT65XX_LED_MODE_NONE, -1, {0}},
+	{"green",             MT65XX_LED_MODE_NONE, -1, {0}},
 	{"blue",              MT65XX_LED_MODE_NONE, -1, {0}},
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1, {0}},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1, {0}},
